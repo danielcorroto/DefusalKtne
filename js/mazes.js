babd77fc95fa97fcdd3c50f5245b47fdc8f11b0c
Daniel Corroto <danicq85@gmail.com>
2016-01-18 20:10:35 +0100

    Mazes

--- js/mazes.js
+++ js/mazes.js
@@ -1,5 +1,7 @@
 ﻿// Onclick dropdown
-$(".dropdown-menu a").click(function (){
+$(".dropdown-menu a").click(function (e){
+	e.preventDefault();
+	
 	var selected = $(this).text();
 	var value = $(this).parents("div.btn-group").find("button span.value");
 	$(value).text(selected);
@@ -11,12 +13,28 @@ function check() {
 	var cells = $("button span.value");
 	var positions = $(cells).map(function(){return $(this).text();}).get();
 	
-	var circle1 = get_positions(positions, 0,1);
-	var circle2 = get_positions(positions, 2,3);
-	var start = get_positions(positions, 4,5);
-	var goal = get_positions(positions, 6,7);
+	var circle = get_positions(positions, 0,1);
+	var start = get_positions(positions, 2,3);
+	var goal = get_positions(positions, 4,5);
 	
-	verify(circle1, circle2, start, goal);
+	var path = verify(circle, start, goal);
+	
+	
+	var sol = "";
+	for (var i=1; i<path.length; i++) {
+		if (path[i] - path[i-1] == -6) {
+			sol += "\u21E7 "; // arriba
+		} else if (path[i] - path[i-1] == 1) {
+			sol += "\u21E8 "; // derecha
+		} else if (path[i] - path[i-1] == 6) {
+			sol += "\u21E9 "; // abajo
+		} else if (path[i] - path[i-1] == -1) {
+			sol += "\u21E6 "; // izquierda
+		}
+	}
+	$("#path").text(sol);
+	
+	$(document).scrollTop(0);
 }
 
 function get_positions(positions, x, y) {
@@ -26,11 +44,10 @@ function get_positions(positions, x, y) {
 
 // Realiza la comprobación
 // circle1 es un array de fila/columna con la posición de un círculo
-// circle2 es un array de fila/columna con la posición de un círculo
 // start es un array de fila/columna con la posición de inicio
 // goal es un array de fila/columna con la posición de fin
 // Devuelve la ruta
-function verify(circle1, circle2, start, goal) {
+function verify(circle, start, goal) {
 	var maze1 = [[1, 6], [0, 2], [1, 8], [4, 9], [3, 5], [4],
 	[0, 12], [8, 13], [2, 7], [3, 10], [9, 11], [10, 17],
 	[6, 18], [7, 14], [13, 20], [16, 21], [15, 17], [11, 16, 23],
@@ -52,68 +69,67 @@ function verify(circle1, circle2, start, goal) {
 	[18, 30], [19, 26], [20, 25], [21, 33], [22, 34], [23, 35],
 	[24, 31], [30, 32], [31, 33], [27, 32], [28, 35], [29, 34]];
 	
-	var maze4 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze4 = [[1, 6], [0, 7], [3], [2, 4], [3, 5], [4, 11],
+	[0, 12], [1, 13], [9, 14], [8, 10], [9, 11], [5, 10, 17],
+	[6, 18], [7, 14], [8, 13], [16, 21], [15], [11, 23],
+	[12, 24], [20], [19, 21], [15, 20, 22], [21, 23], [17, 22, 29],
+	[18, 25, 30], [24, 26], [25, 27], [26, 28], [27, 34], [23, 35],
+	[24, 31], [30, 32], [31], [34], [28, 33], [29]];
 	
-	var maze5 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze5 = [[1], [0, 2], [1, 3], [2, 4], [3, 5, 10], [4, 11],
+	[7, 12], [6, 8], [7, 9], [8, 10, 15], [4, 9], [5],
+	[6, 13, 18], [12, 19], [15], [9, 14], [17, 22], [16, 23],
+	[12, 24], [13, 20], [19, 21], [20, 27], [16], [17, 29],
+	[18, 30], [26, 31], [25, 27], [21, 26, 28], [27], [23, 35],
+	[24], [25, 32], [31, 33], [32, 34], [33, 35], [29, 34]];
 	
-	var maze6 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze6 = [[6], [2, 7], [1, 8], [4], [3, 5, 10], [4, 11],
+	[0, 12], [1, 13], [2, 14], [10, 15], [4, 9], [5, 17],
+	[6, 13, 18], [7, 12], [8], [9, 21], [17, 22], [11, 16],
+	[12, 19], [18, 25], [21, 26], [15, 20, 27], [16, 28], [29],
+	[25, 30], [19, 24], [20], [21, 33], [22, 29], [23, 28],
+	[24, 31], [30, 32], [31, 33], [27, 32], [35], [29, 34]];
 	
-	var maze7 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze7 = [[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], []];
 	
-	var maze8 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze8 = [[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], []];
 	
-	var maze9 = [[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[] ,
-	[] ,[] ,[] ,[] ,[] ,[]];
+	var maze9 = [[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], [],
+	[], [], [], [], [], []];
 	
-	var pos_circle1 = transform_coord_to_pos(circle1);
-	var pos_circle2 = transform_coord_to_pos(circle2);
+	var pos_circle = transform_coord_to_pos(circle);
 	var maze = undefined;
-	if (find_maze(pos_circle1,pos_circle2, 6, 17)) {
+	if (find_maze(pos_circle, 6, 17)) {
 		maze = maze1;
-	} else if (find_maze(pos_circle1,pos_circle2, 10, 19)) {
+	} else if (find_maze(pos_circle, 10, 19)) {
 		maze = maze2;
-	} else if (find_maze(pos_circle1,pos_circle2, 21, 23)) {
+	} else if (find_maze(pos_circle, 21, 23)) {
 		maze = maze3;
-	} else if (find_maze(pos_circle1,pos_circle2, 0, 18)) {
+	} else if (find_maze(pos_circle, 0, 18)) {
 		maze = maze4;
-	} else if (find_maze(pos_circle1,pos_circle2, 16, 33)) {
+	} else if (find_maze(pos_circle, 16, 33)) {
 		maze = maze5;
-	} else if (find_maze(pos_circle1,pos_circle2, 4, 26)) {
+	} else if (find_maze(pos_circle, 4, 26)) {
 		maze = maze6;
-	} else if (find_maze(pos_circle1,pos_circle2, 1, 31)) {
+	} else if (find_maze(pos_circle, 1, 31)) {
 		maze = maze7;
-	} else if (find_maze(pos_circle1,pos_circle2, 3, 20)) {
+	} else if (find_maze(pos_circle, 3, 20)) {
 		maze = maze8;
-	} else if (find_maze(pos_circle1,pos_circle2, 8, 24)) {
+	} else if (find_maze(pos_circle, 8, 24)) {
 		maze = maze9;
 	}
 	
@@ -124,24 +140,11 @@ function verify(circle1, circle2, start, goal) {
 	find_path(maze, path, pos_goal);
 	console.log(path);
 	
-	var sol = "";
-	for (var i=1; i<path.length; i++) {
-		if (path[i] - path[i-1] == -6) {
-			sol += "\u21E7 "; // arriba
-		} else if (path[i] - path[i-1] == 1) {
-			sol += "\u21E8 "; // derecha
-		} else if (path[i] - path[i-1] == 6) {
-			sol += "\u21E9 "; // abajo
-		} else if (path[i] - path[i-1] == -1) {
-			sol += "\u21E6 "; // izquierda
-		}
-	}
-	
-	$("#path").text(sol);
+	return path;
 }
 
-function find_maze(circle1, circle2, pos1, pos2) {
-	return (circle1 == pos1 && circle2 == pos2) || (circle2 == pos1 && circle1 == pos2)
+function find_maze(circle, pos1, pos2) {
+	return (circle == pos1 || circle == pos2);
 }
 
 function transform_coord_to_pos(pos) {
