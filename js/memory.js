b3cf6f59e3a13e4bdd9c43b5650765d889860f5b
Daniel Corroto <danicq85@gmail.com>
2016-02-02 20:05:34 +0100

    Memory

---
+++ js/memory.js
@@ -0,0 +1,8 @@
+﻿// Id de cada una de las etapas
+var stage_1_display = ["#stage_1_display_1", "#stage_1_display_2", "#stage_1_display_3", "#stage_1_display_4"];
+var stage_1_position_pressed = ["#stage_1_position_pressed_1", "#stage_1_position_pressed_2", "#stage_1_position_pressed_3", "#stage_1_position_pressed_4"];
+var stage_1_label_pressed = ["#stage_1_label_pressed_1", "#stage_1_label_pressed_2", "#stage_1_label_pressed_3", "#stage_1_label_pressed_4"];
+var stages = [stage_1_display, stage_1_position_pressed, stage_1_label_pressed];
+
+// Onclick buttons
+set_button_callbacks(stages);
